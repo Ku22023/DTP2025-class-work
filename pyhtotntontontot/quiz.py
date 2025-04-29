7f65356777a6822d8f573b65d1d24208c008cd23
Ku22023 <ku22023@my.westlake.school.nz>
2025-04-29 15:10:25 +1200

    PTUTJGJGODLFMS

---
+++ pyhtotntontontot/quiz.py
@@ -0,0 +1,18 @@
+score = 0
+answer = input(int("what is the sum of 10 and 5"))
+if answer == 15:
+    print("yipee dipee doo!")
+    score += 1
+else:
+    print("sorry u got it wrong")
+answer = input(str("What city has the eiffel tower?"))
+answer = answer.lower()
+if answer == "paris":
+    print("yabbadabbadoo!")
+    score += 1
+else:
+    print("wrong agagin!!!!!")
+answer = input(str("What country is the eiffel tower in?"))
+answer = answer.lower()
+if answer = "france":
+    print("Yahoooo!")
