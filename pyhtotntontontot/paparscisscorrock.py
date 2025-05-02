9f005fad54f3e91886aa62e76f4fa80e5230d74d
Ku22023 <ku22023@my.westlake.school.nz>
2025-05-02 12:00:13 +1200

    RPS

--- pyhtotntontontot/paparscisscorrock.py
+++ pyhtotntontontot/paparscisscorrock.py
@@ -3,27 +3,36 @@ from random import sample
 list = ["r", "p", "s"]
 
 def get_user_input():
-    user_input = str(input("Type P for Paper \n S for Scissor \n R for Rock"))
-    return user_input
+    user_input = str(input("Type:\nP for Paper \nS for Scissor \nR for Rock\n"))
+    if user_input == "p" or user_input == "s" or user_input == "r":
+        return user_input
+    else:
+        print("Please enter a valid argument.")
+
 
 def generate_item():
-    option = sample(list, 1)
-    return option
+    gen_option = sample(list, 1)
+    return gen_option
 
-def whowins(user_input, option, wins, games, lost):
-    if user_input == option:
-        print(f"You chose {user_input}")
+def whowins(user_input, gen_option):
+    if user_input == gen_option:
+        print("you tied!")
+    elif user_input == "r" and gen_option == "s":
+        print("You won!")
+    elif user_input == "s" and gen_option == "p":
+        print("You won!")
+    elif user_input == "p" and gen_option == "r":
+        print("You won!")
     else:
-        if user_input == "r":
-            if option == "s":
-                print("You chose Rock and the Robot chose Scissors! \n You win!")
-                wins += 1
-                return wins
-            elif option == "s"
-    games += 1
-    return games
+        print("you lost!")
+    print(f"Computer chose: {gen_option}")
+    print(f"You chose: {user_input}")
+
 
 games = 0
 wins = 0
 lost = 0
 print(f"Welcome to paper scisscors rock! \n You've won: {wins} \n You've played: {games} \n You've lost: {lost}")
+user_input = get_user_input()
+gen_option = generate_item()
+whowins(user_input,gen_option)
