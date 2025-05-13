77c9ffd069a98f53d56747451bbf02d6c6b2e4e5
Ku22023 <ku22023@my.westlake.school.nz>
2025-05-14 09:50:20 +1200

    added add movie and cleaned up gui

--- python/movie.py
+++ python/movie.py
@@ -44,14 +44,31 @@ users = {
            "balance": 312.75},
 }
 
-def SearchMovie():
+def SearchMovie(userrank):
     print()
     print("Search for Movie:")
+    j = 0
+    print("Type 'Return' to go back")
     searchfor = str(input("Enter the name of the movie you would like to search for: "))
-    if searchfor in movies[]]["title"]:
-        print("success")
+    if searchfor.lower() != "return":
+        for i in movies:
+            if searchfor == movies[i]["title"]:
+                print("Movie found!")
+                print(i)
+                for k in movies[i]:
+                    print(i[k])
+                    print(k, i[k])
+                SearchMovie(userrank)
+            else:
+                j += 1
+                if j == len(movies):
+                    print("No movie found")
+                    SearchMovie(userrank)
     else:
-        print("no movie in there")
+        if userrank == "admin":
+            AdminMenu()
+        else:
+            UserMenu()
 
 def AdminMenu():
     print()
@@ -61,7 +78,7 @@ def AdminMenu():
     print("3. Edit a Movie")
     admin_option = int(input("Enter An option: "))
     if admin_option == 1: #search for movie
-        SearchMovie()
+        SearchMovie(userrank="admin")
     elif admin_option == 2: #add movie
         AddMovie()
     elif admin_option == 3: #edit movie
@@ -70,20 +87,34 @@ def AdminMenu():
         print("Error: Please enter a proper option!")
         AdminMenu()
 
+def UserMenu():
+    print()
+    print("--- Main Menu ---")
+    print("1. Search for Movie")
+    admin_option = int(input("Enter An option: "))
+    if admin_option == 1: #search for movie
+        SearchMovie(userrank="user")
+    else:
+        print("Error: Please enter a proper option!")
+        UserMenu()
+
 def Login():
     print()
+    username = str(input("Enter your Username please: "))
     password = str(input("Enter a password please: "))
-    if password == users["admin"]["password"]:
-        print("welcome admin")
-        AdminMenu()
-    else: #kyle u fix this later
-        print("incorrect password")
+    if username in users:
+        if password == users[username]["password"]:
+            print(f"Welcome {username}!")
+            if username == "admin":
+                AdminMenu()
+            else:
+                UserMenu()
+        else:
+            print("Error: Username or Password Incorrect")
+            Login()
+    else:
+        print("Error: Username or Password Incorrect")
         Login()
-#        for i in users:
-#            if users[i]["password"] == password:
-#                print("welcome", i)
-#            else:
-#                print("wrong")
 
 for i in range(1,100):
     print()
